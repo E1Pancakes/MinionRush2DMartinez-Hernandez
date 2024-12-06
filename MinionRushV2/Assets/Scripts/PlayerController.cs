649f4cdc2e37ebb263564a838fd8c0ddc40b7528
Diego Martinez <145380966+E1Pancakes@users.noreply.github.com>
2024-12-06 11:43:26 -0800

    fixed the anim
    finished the run anim

--- MinionRushV2/Assets/Scripts/PlayerController.cs
+++ MinionRushV2/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
     // Start is called before the first frame update
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,13 +21,10 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if(horizontalInput > 0.01f)
-            transform.localScale = Vector3.zero;
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-50, 50, 1);
-
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);    
+
+        anim.SetBool("Run", horizontalInput != 0);
     }
     
 }
