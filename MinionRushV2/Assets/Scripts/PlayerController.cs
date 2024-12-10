68344cbc164055d11dd60af48715cca428159ac0
Diego Martinez <145380966+E1Pancakes@users.noreply.github.com>
2024-12-10 11:42:51 -0800

    reseting some things

--- MinionRushV2/Assets/Scripts/PlayerController.cs
+++ MinionRushV2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -18,13 +19,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);    
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
-        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
     
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
 }
